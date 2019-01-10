fa4aa43a87085724cd677e860b4dd5ea28d0294d
Thanh Huynh <hu.thanh89@yahoo.com>
2019-01-10 12:50:07 -0800

    Clear settings when cancel

--- game-server/html/instructor/components/setting/setting.js
+++ game-server/html/instructor/components/setting/setting.js
@@ -4,9 +4,20 @@
 
 function Controller($scope, $rootScope){
   
+  // Get initial state settings.
+
   $scope.enableInternet = $rootScope.internetEnabled;
+  
+  // If cancel, restore settings to initial state.
+
+  $scope.cancel = function() {
+    $scope.enableInternet = $rootScope.internetEnabled;
+  }
+
+  // Update root scope and send new settings to server. 
 
   $scope.ok = function() {
+    $rootScope.internetEnabled = $scope.enableInternet;
     ws.send(JSON.stringify({
       type: 'internet',
       enable: $scope.enableInternet
